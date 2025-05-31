d0da772b362d7be5a0bdf8d5051b9a607fea57ad
Abdelhadi360 <abdelhadi11209@gmail.com>
2025-05-31 13:52:02 +0200

    Upload Complete Portfolio

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import InertiaPlugin from "gsap/InertiaPlugin";
 import MotionPathPlugin from "gsap/MotionPathPlugin";
-gsap.registerPlugin(Draggable, InertiaPlugin, MotionPathPlugin);
+gsap.registerPlugin(Draggable, ScrollTrigger, InertiaPlugin);
 
+// Theme Toggle
 
 let sunBtn = document.getElementById("sunBtn");
 let moonBtn = document.getElementById("moonBtn");
@@ -43,6 +45,46 @@ let html = document.documentElement;
     sunBtn.classList.remove("block");
   }
 
+// Side Bar
+let barBtn = document.getElementById("barBtn");
+let xBtn = document.getElementById("xBtn");
+const sideBar = document.getElementById("sideBar");
+const backdrop = document.getElementById("backdrop");
+
+barBtn.addEventListener("click", openSideBar);
+xBtn.addEventListener("click", closeSideBar);
+backdrop.addEventListener("click", closeSideBar);
+
+function openSideBar() {
+  backdrop.classList.remove("hidden");
+
+  gsap.to(backdrop, {
+    duration: .3,
+    opacity: .5
+  });
+
+  gsap.to(sideBar, {
+    duration: .7,
+    opacity: 1,
+    x: 0
+  });
+}
+function closeSideBar() {
+  gsap.to(backdrop, {
+    duration: .3,
+    opacity: 0,
+    onComplete: () => {
+      backdrop.classList.add("hidden");
+    }
+  });
+
+  gsap.to(sideBar, {
+    duration: .7,
+    opacity: 0,
+    x: 384
+  });
+}
+
 // GSAP Animations
 
 const mover = document.getElementById("mover");
@@ -51,7 +93,7 @@ gsap.from("#header a, #header img", {
     duration: .5,
     opacity: 0,
     y: -10,
-    stagger: 0.5
+    stagger: 0.3
 });
 
 gsap.to("#horiz-line", {
@@ -84,13 +126,52 @@ gsap.from(".content .cont-elmnt", {
     stagger: 1
 });
 
+let currentZ = 30;
+const minZ = 30;
+const maxZ = 40;
+Draggable.create(".tools img", {
+    type: "x,y",
+    zIndexBoost: false,
+    bounds: ".tools",
+    inertia: true,
+    onPress: function () {
+
+      currentZ++
+      if(currentZ > maxZ) currentZ = minZ
+
+      gsap.set(this.target, {
+        zIndex: currentZ
+      });
+    }
+});
+
 gsap.from(".tools img", {
     duration: 1.5,
     opacity: 0,
     ease: "none"
 });
-Draggable.create(".tools img", {
-    type: "x,y",
-    bounds: ".tools",
-    inertia: true
-})
+gsap.to(".scrol-bar", {
+    scrollTrigger: {
+        trigger: "#about",
+        start: "top 80%",
+        end: "bottom 90%",
+        scrub: true
+    },
+    duration: 1,
+    height: "100%",
+    ease: "none"
+});
+
+
+gsap.from(".cards div", {
+    scrollTrigger: {
+        trigger: "#skills",
+        start: "top 80%",
+        end: "bottom 90%",
+        // markers: true
+    },
+    duration: 1.5,
+    opacity: 0,
+    y: 100,
+    ease: "none"
+});
